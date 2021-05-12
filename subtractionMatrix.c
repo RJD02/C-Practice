2869fef34a1d8c556135c784ad374b75b4071139
Raviraj Dulange <redhood.dumb@gmail.com>
2021-05-12 12:16:04 +0530

    Second problem

---
+++ subtractionMatrix.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+#define size 3
+
+int main() {
+	int m1[size][size];
+	int m2[size][size];
+	for(int i = 0; i < size; i++)
+		for(int j = 0; j < size; j++)
+			scanf("%d", &m1[i][j]);
+	for(int i = 0; i < size; i++)
+		for(int j = 0; j <size; j++)
+			scanf("%d", &m2[i][j]);
+	for(int i = 0; i < size; i++) {
+		for(int j = 0; j <size; j++) {
+			printf("%d ", m1[i][j] - m2[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
